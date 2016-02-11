33db16cf5b0fc1078d897e0d58638ac9a0394ed3
atddev <ataldalooj@unomaha.edu>
2016-02-10 17:02:05 -0800

    Module 12: Using the Location API

--- workshop/platforms/android/assets/www/js/app.js
+++ workshop/platforms/android/assets/www/js/app.js
@@ -7,10 +7,23 @@
 EmployeeListView.prototype.template = 
             Handlebars.compile($("#employee-list-tpl").html());
 
-   
+   EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
     var service = new EmployeeService();
-    service.initialize().done(function () {
-        $('body').html(new HomeView(service).render().$el);
+
+    var slider = new PageSlider($('body'));
+
+        service.initialize().done(function () {
+      router.addRoute('', function() {
+          slider.slidePage(new HomeView(service).render().$el);
+      });
+
+      router.addRoute('employees/:id', function(id) {
+          service.findById(parseInt(id)).done(function(employee) {
+              slider.slidePage(new EmployeeView(employee).render().$el);
+          });
+      });
+
+      router.start();
     });
     /* --------------------------------- Event Registration -------------------------------- */
     $('.search-key').on('keyup', findByName);
